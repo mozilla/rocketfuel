3633ceb0e8f665b94fff37a7342fcde0c04c50da
Chris Van <cvan@mozilla.com>
2013-08-08 13:53:25 -0700

    new design

--- src/media/js/routes_api.js
+++ src/media/js/routes_api.js
@@ -1,7 +1,8 @@
 define('routes_api', [], function() {
     return {
-    	'collections': '/api/v1/rocketfuel/collections/',
-    	'collection': '/api/v1/rocketfuel/collections/{0}/',
-    	'add_app': '/api/v1/rocketfuel/collections/{0}/add_app'
+        'collections': '/api/v1/rocketfuel/collections/',
+        'collection': '/api/v1/rocketfuel/collections/{0}/',
+        'add_app': '/api/v1/rocketfuel/collections/{0}/add_app',
+        'login': '/api/v1/account/login/'
     };
 });
