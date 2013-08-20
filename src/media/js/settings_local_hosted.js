5b1c7bcc9165bb32a2cffaedc7fd558864a55f86
Matt Basta <me@mattbasta.com>
2013-08-20 14:19:47 -0700

    Add settings local hosted to fix deploy

---
+++ src/media/js/settings_local_hosted.js
@@ -0,0 +1,7 @@
+define('settings_local', [], function() {
+    var origin = window.location.origin || (
+        window.location.protocol + '//' + window.location.host);
+    return {
+        api_url: origin
+    };
+});
