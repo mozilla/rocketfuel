c6ae43e8d9d19a10e67fa669901bcfb3ad1a785d
Matt Basta <me@mattbasta.com>
2013-08-23 12:03:12 -0700

    App incompatibility messages

--- src/media/js/settings.js
+++ src/media/js/settings.js
@@ -10,6 +10,7 @@ define('settings', ['l10n', 'settings_local', 'underscore'], function(l10n, sett
         storage_version: '0',
 
         param_whitelist: ['carrier', 'region', 'category'],
+        api_param_blacklist: ['region'],
 
         model_prototypes: {
             // Dummy prototypes to facilitate testing
--- src/media/js/views/collection.js
+++ src/media/js/views/collection.js
@@ -13,7 +13,11 @@ define('views/collection',
         e.preventDefault();
         var list = $('#app_search_results');
         requests.get(
-            utils.urlparams(urls.api.url('search'), {'q': $('#app_search input[type=search]').val()})
+            utils.urlparams(
+                urls.api.url('search'),
+                {'q': $('#app_search input[type=search]').val(),
+                 'region': 'None'}
+            )
         ).done(function(data) {
             list.html('');
             data.objects.forEach(function(v) {
@@ -21,6 +25,11 @@ define('views/collection',
                 list.append('<li>' + nunjucks.env.getTemplate('helpers/app.html').render(
                     {'this': v, 'allow_delete': true}));
             });
+
+            var collection = collection_model.lookup($('.main').data('id'));
+            apply_incompat(collection.region, data.objects);
+        }).fail(function() {
+            notification.notification({message: gettext('Search failed :(')});
         });
 
     }).on('click', '#app_search .close', function(e) {
@@ -63,7 +72,7 @@ define('views/collection',
         var collection = collection_model.lookup($('.main').data('id'));
         var app_data = app_model.lookup(app + '', 'id');
         var $app = nunjucks.env.getTemplate('helpers/app.html').render(
-            {'this': app_data, 'allow_delete': true, 'allow_reorder': true});
+            {'this': app_data, 'allow_delete': true, 'allow_reorder': true, 'tag': 'li'});
         $('.main ul.apps').append($app);
 
         requests.post(
@@ -113,6 +122,25 @@ define('views/collection',
 
     });
 
+    function apply_incompat(base_region, app_list) {
+        var region_slug = models('region').lookup(base_region).slug;
+        app_list.forEach(function(v) {
+            for (var r in v.regions) {
+                if (v.regions[r].slug === region_slug) {
+                    return;
+                }
+            }
+            var $app = $('.app[data-id="' + v.id + '"]');
+            if ($app.data('incompatible')) {
+                return;
+            }
+            $app.append(
+                '<p class="incompatible">' +
+                gettext("Incompatible: App not available in the collection's region."));
+        });
+
+    }
+
     return function(builder, params) {
         builder.start(
             'collection.html',
@@ -123,6 +151,10 @@ define('views/collection',
             data.apps.forEach(function(v) {
                 app_model.cast(v);
             });
+
+            if (data.region) {
+                apply_incompat(data.region, data.apps);
+            }
         });
 
         builder.z('type', 'leaf');
