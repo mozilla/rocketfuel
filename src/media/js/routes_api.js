efd344169acef99c26a10bc4ed8935e1e528848c
Matt Basta <me@mattbasta.com>
2013-09-26 11:22:01 -0700

    Fix broken remove curator URL

--- src/media/js/routes_api.js
+++ src/media/js/routes_api.js
@@ -5,7 +5,7 @@ define('routes_api', [], function() {
         'collection_image': '/api/v1/rocketfuel/collections/{0}/image/',
         'collection_curators': '/api/v1/rocketfuel/collections/{0}/curators/',
         'add_curator': '/api/v1/rocketfuel/collections/{0}/add_curator/',
-        'remove_curator': '/api/v1/rocketfuel/collections/{0}/add_curator/',
+        'remove_curator': '/api/v1/rocketfuel/collections/{0}/remove_curator/',
         'add_app': '/api/v1/rocketfuel/collections/{0}/add_app/',
         'remove_app': '/api/v1/rocketfuel/collections/{0}/remove_app/',
         'reorder_apps': '/api/v1/rocketfuel/collections/{0}/reorder/',
