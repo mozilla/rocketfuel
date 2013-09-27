a17a2e4866be8e93318bbefc3afea45fd0595f1a
Alexandros Mioglou <alexandros@mioglou.me>
2013-09-27 07:25:01 +0300

    Added el locale to track greek l10n effort

--- locale/omg_create_locales.sh
+++ locale/omg_create_locales.sh
@@ -1,4 +1,4 @@
-LOCALES=(bg ca cs de en_US es eu fr ga_IE hr hu it ja nl pl pt_BR ru sk zh_TW dbg)
+LOCALES=(bg ca cs de el en_US es eu fr ga_IE hr hu it ja nl pl pt_BR ru sk zh_TW dbg)
 POT_FILE="locale/templates/LC_MESSAGES/messages.pot"
 INIT_FLAGS="--input=$POT_FILE --width=200 --no-translator"
 LANGPACK_DIR="src/media/locales"
